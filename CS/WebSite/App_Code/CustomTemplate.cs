6d76a82380bf225ed3b6a019703a0ee76eaf87be
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/App_Code/CustomTemplate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using DevExpress.Web.ASPxGridView;
+using DevExpress.Web.ASPxEditors;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class CustomTemplate : ITemplate {
+
+	public void InstantiateIn(Control container) {
+		GridViewDataItemTemplateContainer c = container as GridViewDataItemTemplateContainer;
+		if (c.Column is GridViewDataCheckColumn) {
+			object o = DataBinder.Eval(c.DataItem, c.Column.FieldName);
+			if ((o != null) && ((bool)o)){
+				ASPxCheckBox cb = new ASPxCheckBox {
+					ID = "myCB",
+					Checked = true
+				};
+			c.Controls.Add(cb);
+			}				
+		}
+		else {
+			c.Controls.Add(
+				new LiteralControl(DataBinder.Eval(c.DataItem, c.Column.FieldName).ToString())
+			);
+		}
+	}
+}
