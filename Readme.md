fd60cc34fc2ec88b3589eed73ff62677c52742a8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to implement a dynamical DataItem template for several same-type columns
+
+
+<p>This example demonstrates how to implement dynamical DataItem template for several columns of the same type. In this example ASPxCheckBox is shown only if its cell contains the true value. If additional GridViewDataCheckBoxColumns are appended, the template will be applied to them as well.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub grid_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim g As ASPxGridView = TryCast(sender, ASPxGridView)
+		g.Templates.DataItem = New CustomTemplate()
+	End Sub
+End Class
